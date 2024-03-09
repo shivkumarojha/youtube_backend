d9943ab07ddf4df3baf2ba7376c356e4696a30be
shivkumarojha <ojhashivkumar88@gmail.com>
2024-03-09 14:16:13 +0530

    added cors also json config for data

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from 'cors'
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+// Cors middleware
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+
+// For accepting JSON
+app.use(express.json({limit: "16kb"}))
+
+
+
+export default app
--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,9 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
         console.log(`\n MongoDb connected!! DB HOST: ${connectionInstance.connection.host}`)
+
     } catch(error) {
         console.log("Mongo error: ", error)
         process.exit(1)
