f88dd4c54134bbdb1858f2e94d642efaf943cfac
shivkumarojha <ojhashivkumar88@gmail.com>
2024-03-09 17:47:43 +0530

    created user and video models

---
+++ src/models/user.models.js
@@ -0,0 +1,52 @@
+import mongoose, { Schema, mongo } from "mongoose";
+import jwt from 'jsonwebtoken'
+import bcrypt from "bcrypt"
+
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String, //cloudinary url
+    },
+    coverImage: {
+        type: String
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    refereshToken: {
+        type: String
+    }
+}, {
+    timestamps: true
+})
+
+
+export const user = mongoose.model('User', userSchema)
