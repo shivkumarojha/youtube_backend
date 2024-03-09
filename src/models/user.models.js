c67fcdc22d81c48bc009b5c8395aaa04574a3bbf
shivkumarojha <ojhashivkumar88@gmail.com>
2024-03-09 18:13:20 +0530

    user schema methods for access and refresh tokens

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -48,5 +48,45 @@ const userSchema = new Schema({
     timestamps: true
 })
 
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next()
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+// Check the user password:--- Revalidate this code- IN CONFUSION
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+//  Generate Access Token
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+      {
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+      }
+    );
+}
+
+// Generate Refresh Token
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+      {
+        _id: this._id,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+      }
+    );
+}
 
 export const user = mongoose.model('User', userSchema)
