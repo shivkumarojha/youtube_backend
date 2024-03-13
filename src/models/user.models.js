b2f5770bce5c42480163a3e81164eb53347ecb9f
shivkumarojha <ojhashivkumar88@gmail.com>
2024-03-13 18:03:02 +0530

    fixed register controller bugs

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -19,7 +19,7 @@ const userSchema = new Schema({
         lowercase: true,
         trim: true
     },
-    fullname: {
+    fullName: {
         type: String,
         required: true,
         trim: true,
@@ -67,7 +67,7 @@ userSchema.methods.generateAccessToken = function() {
         _id: this._id,
         email: this.email,
         username: this.username,
-        fullname: this.fullname,
+        fullName: this.fullName,
       },
       process.env.ACCESS_TOKEN_SECRET,
       {
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -8,21 +8,25 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+
+
+
 const uploadOnCloudinary = async(localFilePath) => {
     try {
-        if(!localFilePath) {
-            return "Local File Path Not Found"
-        }
+        console.log("cloudinary ", localFilePath)
+        if(!localFilePath) return null
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
+        
         // File Successfully uploaded
         console.log("File is uploaded on cloudinary ", response.url)
+        fs.unlinkSync(localFilePath);
         return response
     }
 
     catch (error) {
-        fs.unlink(localFilePath) // Remove locally saved temporary file after complete up
+        fs.unlinkSync(localFilePath) // Remove locally saved temporary file after complete up
         return null
     }
 }
