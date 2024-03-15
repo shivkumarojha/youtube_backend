8dafcb805ca17a2fb618a227dac3dc87c2d9982f
shivkumarojha <ojhashivkumar88@gmail.com>
2024-03-15 19:32:48 +0530

    fix: added routes in user route for controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,7 +4,13 @@ import {
   registerUser,
   logoutUser,
   refreshAccessToken,
-  changeCurrentPassword
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory
 } from "../controllers/user.controller.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -38,4 +44,24 @@ userRouter.route("/refresh-token").post(refreshAccessToken)
 
 // change user Password
 userRouter.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+// Get current user
+userRouter.route("/current-user").get(verifyJWT, getCurrentUser)
+
+//  Update Account details
+userRouter.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+// update user avatar
+userRouter.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+// update cover Image
+userRouter.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+// get Channel profile
+userRouter.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+// get watch history
+userRouter.route("/watch-history").get(verifyJWT, getWatchHistory)
+
+
 export { userRouter }; 
